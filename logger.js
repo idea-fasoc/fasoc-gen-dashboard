72ec465a7bbec36c2837ab0e2805937f9bc6efb8
Zineb Benameur pro <zinebbenameurpro@Zinebs-MacBook-Pro.local>
2021-02-03 23:35:08 -0500

    fix production bug

--- logger.js
+++ logger.js
@@ -2,5 +2,5 @@ const pino = require('pino');
 
 
 
-const logger = pino({ prettyPrint: process.env.NODE_ENV === 'production' });
+const logger = pino({ prettyPrint: process.env.NODE_ENV === 'development' });
 module.exports = logger;
