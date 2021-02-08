b77196e22c0eed25c6f8146dd39828d4b2f50720
Zineb Benameur pro <zinebbenameurpro@Zinebs-MacBook-Pro.local>
2021-02-08 00:56:58 -0500

    cleaning unused files

--- app.js
+++ app.js
@@ -9,18 +9,6 @@ const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const RedisStore = require('connect-redis')(session);
 
-//Import the mongoose module
-var mongoose = require('mongoose');
-
-//Set up default mongoose connection
-var mongoDB = 'mongodb://localhost:27017/Fasoc';
-mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
-
-//Get the default connection
-var db = mongoose.connection;
-
-//Bind connection to error event (to get notification of connection errors)
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 const initAuthMiddleware = require('./features/login/init-auth-middleware');
 const indexRouter = require('./routes/index');
